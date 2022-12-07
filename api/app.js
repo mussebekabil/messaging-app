fed3cff7bfc9e6ea3ddd36805a14f7df23114f45
mussebekabil <musse.bekabil@nordcloud.com>
2022-12-08 00:48:14 +0200

    infinite scroll support

--- api/app.js
+++ api/app.js
@@ -1,12 +1,18 @@
 import { serve } from "./deps.js";
 import { handleGetRequests, handlePostRequests, handlePatchRequests } from './handlers.js'
 
+const toJson = (data) => {
+  if (data !== undefined) {
+      return JSON.stringify(data, (_, v) => typeof v === 'bigint' ? `${v}#bigint` : v)
+          .replace(/"(-?\d+)#bigint"/g, (_, a) => a);
+  }
+}
 const handleRequest = async (request) => {
   switch(request.method){
     case 'POST':
       return handlePostRequests(request);
     case 'GET':
-      return new Response(JSON.stringify(await handleGetRequests(request)));
+      return new Response(toJson(await handleGetRequests(request)));
     case 'PATCH': 
       return handlePatchRequests(request);
     default:
--- ui/src/components/SingleMessage.jsx
+++ ui/src/components/SingleMessage.jsx
@@ -1,24 +1,47 @@
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 import { openWsConnection, closeWsConnection } from '../utils/wsUtil.js';
 import MessageForm from './MessageForm.jsx';
 import Card from './Card.jsx';
 import * as constants from '../utils/constants.js';
+import useFetch from '../../hooks/useFetch.js';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 const SingleMessage = ({ message }) => {
-	const [replies, setReplies] = useState([]);
 	const [safeMessage, setSafeMessage] = useState({});
+	const [offset, setOffset] = useState(0);
+	const { 
+		isLoading, 
+		error, 
+		items, 
+		setItems, 
+		hasMore 
+	} = useFetch(`/api/replies/${message?.id}`, offset);
+	const observer = useRef();
+	
+	const lastMessageElementRef = useCallback((node) => {
+		if (isLoading) return;
+		if (observer.current) observer.current.disconnect();
+		observer.current = new IntersectionObserver((entries) => {
+			if (entries[0].isIntersecting && hasMore) {
+				setOffset((prev) => prev + 20);
+			}
+		});
+		if (node) observer.current.observe(node);
+	}, [isLoading]);
 
 	const handleMessage = (messageEvent) => {
 		const data = JSON.parse(messageEvent.data)
 			switch (data.type) {
 				case constants.REPLY_VOTE_TYPE:
-					const updatedReplies = replies.map(r => r.id == data.replyId ? {...r, vote: data.vote} : r)
-					setReplies(updatedReplies)
+					const updatedReplies = items.map(r => r.id == data.replyId ? {...r, vote: data.vote} : r)
+					setItems(updatedReplies)
 					break;
 				case constants.REPLY_TYPE:
-					setReplies([
+					setItems([
 						data,
-						...replies
+						...items
 					])
 					break;
 				case constants.MESSAGE_VOTE_TYPE: 
@@ -33,26 +56,19 @@ const SingleMessage = ({ message }) => {
 
 	useEffect(() => {
 		openWsConnection(handleMessage);
-	}, [replies])
 
-	useEffect(() => {
-		setSafeMessage(message)
-	}, [message])
-	
-	useEffect(() => {
-		async function fetchReplies () {
-			const response = await fetch(`/api/replies/${safeMessage.id}`);
-			const { replies } = await response.json();
-			setReplies(replies)
-		}
-		fetchReplies()
-		
 		return () => {
 			console.log('unmounted')
 			closeWsConnection()
 		}
-	}, [])
+	}, [items])
+
+	useEffect(() => {
+		setSafeMessage(message)
+	})
 	
+	if(error) toast(error.toString())
+
 	return (
     <div>
 			<Card {...safeMessage} />
@@ -60,12 +76,23 @@ const SingleMessage = ({ message }) => {
 				<MessageForm placeholder="Reply your thoughts" messageId={safeMessage.id}/>
 			</div>
 			<ul role="list" className="link-card-grid">
-				{replies?.map((reply) => (
-					<li key={reply.id}>
-						<Card {...reply} />
-					</li>
-				))}
+				{items?.map((reply, index) => {
+					if(items.length === index + 1) {
+						return (
+							<li key={reply.id} ref={lastMessageElementRef}>
+								<Card {...reply} />
+							</li>
+						)
+					} 
+					return (
+						<li key={reply.id}>
+							<Card {...reply} />
+						</li>
+					)
+				})}
 			</ul>
+			<div>{isLoading && "Loading..."}</div>
+			<ToastContainer />
 		</div>		
 	)
 };
